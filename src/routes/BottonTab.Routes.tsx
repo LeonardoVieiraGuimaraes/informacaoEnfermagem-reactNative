3fa85e4582b6f38fb9174ff7a352d84c7e76d695
leona <leonardovieiraxy@hotmail.com>
2024-06-13 09:06:14 -0300

    Changes to be committed: 	modified:   src/routes/BottonTab.Routes.tsx 	modified:   src/screens/AboutScreen.tsx

--- src/routes/BottonTab.Routes.tsx
+++ src/routes/BottonTab.Routes.tsx
@@ -3,7 +3,7 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 import { AntDesign } from '@expo/vector-icons';
 import AboutScreen from '../screens/AboutScreen';
 import StackRoutes from './Stack.Routes';
-import ApoioScreen from '../screens/ApoioScreen';
+// import ApoioScreen from '../screens/ApoioScreen';
 
 interface Screen {
   name: string;
@@ -14,7 +14,7 @@ interface Screen {
 const screens: Screen[] = [
   { name: 'Home', component: StackRoutes, icon: 'home' },
   { name: 'Sobre', component: AboutScreen, icon: 'infocirlceo' },
-  { name: 'Apoio', component: ApoioScreen, icon: 'infocirlceo' },
+  // { name: 'Apoio', component: ApoioScreen, icon: 'infocirlceo' },
 ];
 const Tab = createMaterialBottomTabNavigator();
 
--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -5,22 +5,23 @@ import styles from '../assets/styles/styles';
 export default function AboutScreen() {
   return (
     <View className={styles.viewAbout}>
-      <Text className={styles.textTitle}>
-        {'Bem Vindo ao A&G Enfermagem (Autonomia e Gestão na Enfermagem)'}
-      </Text>
-
-      <Text className="text-lg text-justify">
+      <View className={styles.viewTitle}>
+        <Text className={styles.textTitle}>
+          {'Bem Vindo ao A&G Enfermagem (Autonomia e Gestão na Enfermagem)'}
+        </Text>
+      </View>
+      <Text className="text-sm text-justify">
         {
           'Este é um instrumento para subsidiar o seu processo de trabalho por meio de informações gerais com o objetivo de apresentar as estratégias para alcançar o melhor nível de autonomia e contribuir com a gestão da enfermagem.\n'
         }
       </Text>
-      <Text className="text-lg  text-center font-bold">{'Produção:'}</Text>
-      <Text className="text-lg  text-center">
+      <Text className="text-sm  text-center font-bold">{'Produção:'}</Text>
+      <Text className="text-sm text-center">
         {
           'Leslie Diniz Alves\nLeonardo Vieira Guimarães \nSérgio Valverde M. Santos'
         }
       </Text>
-      <Text className="text-lg  text-center font-bold">{'\nApoio:'}</Text>
+      <Text className="text-sm  text-center font-bold">{'\nApoio:'}</Text>
 
       <Image
         source={require('../assets/img/Logos2.jpg')}
